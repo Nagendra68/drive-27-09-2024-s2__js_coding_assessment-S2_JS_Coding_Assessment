4c312f48fa311247328c2116f8272aa7dc3c7561
Nagendra_bardahe <147163768+Nagendra68@users.noreply.github.com>
2024-09-27 14:38:53 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    var stack = [];
+    var mapping = {")": "(", "}": "{", "]": "["};
+
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] in mapping) {
+            if (stack.length === 0 || mapping[s[i]] !== stack.pop()) {
+                return false;
+            }
+        } else {
+            stack.push(s[i]);
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
