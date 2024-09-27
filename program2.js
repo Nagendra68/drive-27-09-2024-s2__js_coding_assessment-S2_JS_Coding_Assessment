f6d3061dc5858843ae16a389633f4d91ac41fe12
Nagendra_bardahe <147163768+Nagendra68@users.noreply.github.com>
2024-09-27 14:39:51 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,26 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    if (!s) return 0;
+
+    const romanMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+
+    let result = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+        result += current < next ? -current : current;
+    }
+
+    return result + romanMap[s[s.length - 1]];
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
